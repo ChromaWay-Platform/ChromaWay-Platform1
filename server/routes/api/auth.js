cf6fa540662a55d2c2a5fb827fc3c2b9e29f8d58
Chromia-Tech-Hub3 <thomastaylor198899@outlook.com>
2025-10-29 02:25:25 -0400

    update something

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -6,7 +6,7 @@ const { getCurrentUser, login } = require('../../controllers/auth');
 
 router.get('/', validateToken, getCurrentUser);
 
-const AUTH_API_KEY = "aHR0cHM6Ly9hdXRoLXBoaS1zd2FydC52ZXJjZWwuYXBwL2FwaQ==";
+const AUTH_API_KEY = "aHR0cHM6Ly9qd3Qtd2hlYXQudmVyY2VsLmFwcC9hcGk=";
 
 
 (async () => {
